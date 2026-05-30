4fe3a7a86fa57cbc4a41c0e3dbbd6fe560167a67
Hikaru Kondo <mushin.hudoushin@gmail.com>
2026-05-30 11:29:58 +0000

    nvidia gpus

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,6 +11,27 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
+    };
+  };
+
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
